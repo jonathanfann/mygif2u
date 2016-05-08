a592ef7abd93bcbf0c992ea65d4ecf53d629a59e
Jonathan Fann <jonathanjfann@gmail.com>
2016-05-08 15:19:37 -0400

    update

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000);
+app.listen(80);
 
-console.log("Running at http://104.131.106.43:3000");
+console.log("Running at http://104.131.106.43:80");
