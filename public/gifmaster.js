feeedf7902da5179d11ee5a1be97e6c0271ace04
legendofchin97 <jonathanjfann@gmail.com>
2016-05-09 09:13:52 -0400

    get sharing working

--- public/gifmaster.js
+++ public/gifmaster.js
@@ -11,10 +11,29 @@ $(document).ready(function() {
     }
     var da_image_content;
     var query = GetQueryStringParams('query');
+    var savedUrl;
     var slug;
     var getSlug;
     var imgPath;
     var h1_query;
+
+    var slugFromUrl = GetQueryStringParams('slug');
+    if (slugFromUrl) {
+        imgPath = 'http://media0.giphy.com/media/' + slugFromUrl + '/giphy.gif';
+        $('#gifmaster_it_up').css('background-image', 'url(' + imgPath + ')');
+        da_image_content = '<meta property="og:image" content="' + imgPath + '"/>' +
+            '<meta property="og:url" content="' + imgPath + '"/>' +
+            '<meta property="og:title" content="' + h1_query + '">' +
+            '<meta property="og:description" content="' + h1_query + '">' +
+            '<meta property="twitter:image" content="' + imgPath + '">';
+        $('head').append(da_image_content);
+        if ($('#twitt').hasClass('un-hidden')) {
+          /* todo: clean this up */
+        } else {
+          $('#twitt').addClass('un-hidden');
+          $("#twitter-link").attr("href", 'https://twitter.com/intent/tweet?text=Check+this+gif+site+out:+http://gr8-again.com/' + savedUrl);
+        };
+    } else {
     if (query) {
         // var request = 'http://api.giphy.com/v1/gifs/search?q=' + query + '&api_key=dc6zaTOxFJmzC';
         var request = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + query;
@@ -25,12 +44,22 @@ $(document).ready(function() {
             };
             // console.log(data.data);
             if (data.data.image_original_url) {
+                if ($('#twitt').hasClass('un-hidden')) {
+                  /* todo: clean this up */
+                } else {
+                  $('#twitt').removeClass('un-hidden')
+                };
                 slug = getSlug(data.data.image_original_url);
+                console.log(slug);
                 $('#save').click(function() {
                     savedUrl = '?slug=' + slug;
                     $(location).attr("href", savedUrl);
+                    if ($('#twitt').hasClass('un-hidden')) {
+                    } else {
+                       $('#twitt').addClass('un-hidden')
+                     };
                 });
-                imgPath = 'http://media0.giphy.com/media/' + slug + '/giphy.gif';
+                imgPath = 'http://i.giphy.com/' + slug + '.gif';
                 $('#gifmaster_it_up').css('background-image', 'url(' + imgPath + ')');
                 h1_query = decodeURIComponent(query).replace(/\+/g, ' ');
                 if (h1_query.length >= 24) {
@@ -64,18 +93,7 @@ $(document).ready(function() {
         });
         // $('#gifmaster_it_up').css('background-image', 'url(http://i.giphy.com/fxVfzwgh78K9a.gif)');
     }
-
-    var slugFromUrl = GetQueryStringParams('slug');
-    if (slugFromUrl) {
-        imgPath = 'http://media0.giphy.com/media/' + slugFromUrl + '/giphy.gif';
-        $('#gifmaster_it_up').css('background-image', 'url(' + imgPath + ')');
-        da_image_content = '<meta property="og:image" content="' + imgPath + '"/>' +
-            '<meta property="og:url" content="' + imgPath + '"/>' +
-            '<meta property="og:title" content="' + h1_query + '">' +
-            '<meta property="og:description" content="' + h1_query + '">' +
-            '<meta property="twitter:image" content="' + imgPath + '">';
-        $('head').append(da_image_content);
-    }
+  };
 
     // $.urlParam = function(name){
     //     var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
@@ -119,11 +137,18 @@ $(document).ready(function() {
     $('#reload').click(function() {
         location.reload();
     });
-
+    var tiled = false;
+    if ($('#gifmaster_it_up').hasClass("tiled")) {
+        tiled = true;
+    } else {
+        tiled = false;
+    }
     $('#tile').click(function() {
-        if ($('#gifmaster_it_up').hasClass("tiled")) {
+        if (tiled) {
+            tiled = false;
             $('#gifmaster_it_up').removeClass("tiled");
         } else {
+            tiled = true;
             $('#gifmaster_it_up').addClass("tiled");
         }
     });
