9fa3e6decb0d5b12bc9488050a0391f67625bc88
legendofchin97 <jonathanjfann@gmail.com>
2016-05-11 08:05:26 -0400

    angular

--- public/controllers.js
+++ public/controllers.js
@@ -1,3 +1,22 @@
-var app = angular.module('myApp', [
+angular.module('myApp', [])
+    .controller('gr8Controller', ['$scope', function($scope) {
+        $scope.q = 'Search';
+        $scope.doSubmit = function(q, query) {
+            console.log(query);
+            q = query;
+            console.log(q);
+        };
+    }]);
 
-]);
+app.controller('SomeController', ['$scope', 'emails', function($scope, emails) {
+    emails.success(function(data) {
+
+        $scope.emails = data;
+    });
+
+    $scope.myName = "John";
+    $scope.update = function(name) {
+        $scope.myName = name;
+    };
+
+}]);
